3fc111ae5d1908baafaf19525d039dcb74027858
Berler <taliakberler@gmail.com>
2025-12-14 09:45:25 -0500

    Portfolio website template

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
+import { Toaster } from "@/components/ui/toaster";
 
 function App() {
   return (
