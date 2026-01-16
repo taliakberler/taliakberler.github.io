bb9f4b00e0a7a24c681b5808e98a794b72bb9bc4
Berler <taliakberler@gmail.com>
2026-01-16 13:06:10 -0500

    fixed asset reference issues

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -34,7 +34,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="../assets/TaliaBerlerCV.pdf"
+                href="/TaliaBerlerCV.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
